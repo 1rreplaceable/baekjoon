11fd2424c8a0af15023a46e6866483d635f89e90
Seok Jin <97454046+1rreplaceable@users.noreply.github.com>
2023-08-07 17:48:45 +0900

    baekjoon

---
+++ rectri/rt_15894.java
@@ -0,0 +1,12 @@
+package rectri;
+
+import java.util.Scanner;
+
+public class rt_15894 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+
+		System.out.println(4 * n);
+	}
+}
